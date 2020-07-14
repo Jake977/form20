fa8d6399cc6595e8bff5c61a04cb7917a4d1a287
Eugene <openmind1377@gmail.com>
2020-07-14 19:30:37 +0300

    remove console.log's

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-//import './index.scss';
-//import './components/App/App';
-
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 import App from './components/App/App';
 import './styles.scss';
-
-// setup fake backend
-//import { configureFakeBackend } from './_helpers';
-//configureFakeBackend();
 
 render(
     <BrowserRouter>
